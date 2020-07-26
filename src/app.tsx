7241da457867df6104ae4206d4abfe2c50a20e0e
larrycustodio <larry@helloextend.com>
2020-07-25 23:17:14 -0700

    Update to use webpack build; pending frontend

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import { GlobalStyle } from './components/GlobalStyle'
+
+const App: FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <main>
+        <header>
+          <h2>
+            Coming soon!
+          </h2>
+        </header>
+      </main>
+    </>
+  )
+}
+
+export default App
